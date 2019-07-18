0b3c43fa2b4b693608b3f7d560c2944bcb41ce13
Partagetonhot <53058208+Partagetonhot@users.noreply.github.com>
2019-07-18 21:28:39 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,7 @@
-
+// config.js
+module.exports = {
+  consumer_key: 'EM0QOgLhND9PRBxmgJ2qkWTDK',
+  consumer_secret: 'YuhwFNYy84ehKMNJ2zggIYpPjjWora9DM70L8H40yyvMZJnxgZ',
+  access_token_key: '1115692439329619968-BVtRLBvVsl4fAcW1b5U6fiCai9pVX2',
+  access_token_secret: 'JevuITjZQOueE2hrykiAeHyaQvZa2hbrSRROwniFJFB5P'
+}
